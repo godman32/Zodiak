5dc092e333c8cca107d126493ca8beada083b36a
Godman Siagian <godmansiagian32@gmail.com>
2023-07-05 19:03:07 +0700

    first

---
+++ app/src/main/java/com/gm/zodiak/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gm.zodiak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
